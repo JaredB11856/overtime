7016cfa3c2b87c44ebeb722f659be6a38f80680a
Jared Brown <Jb_90210@hotmail.com>
2018-03-31 01:01:32 +0000

    Integrated growl notifications for active record validation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require rails-ujs
+
 //= require bootstrap-sprockets
+//= require gritter
+
 //= require turbolinks
 //= require_tree .
